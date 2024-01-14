91ba4ca677c819f15001053d413d4cb0f59f5408
opestro <mahdiharzallah21@gmail.com>
2024-01-14 17:26:57 +0100

    up

--- pages/apps/ecommerce/product/[slug].vue
+++ pages/apps/ecommerce/product/[slug].vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+const route = useRoute()
 // common components
 import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
 import UiParentCard from '@/components/shared/UiParentCard.vue';
 definePageMeta({
-  middleware: ['auth'],
+    middleware: ['auth'],
 })
 //Tables
 import EditableTable from '@/components/table/EditableTable.vue';
@@ -23,26 +24,30 @@ const breadcrumbs = ref([
         href: ''
     }
 ]);
-let pageNumber: number = 1
-let pageN: number = 0
-let categories: any = []
-//let {products}: any  = []
-async function productsNavigation(page: number) {
-   const products = await useLazyFetch('https://gama.soluve.cloud/products', {
-        params: { 'per_page': 50, 'page': page}
-    })
-    const getCategories = await useLazyFetch('https://gama.soluve.cloud/categories', {
-        params: { 'per_page': 100, 'page': 1}
-    })
-    categories = getCategories.data
-    pageNumber = page
-    return {products, pageN, pageNumber,categories}
-}
-async function productsPagination(data: any) {
-    return navigateTo('/apps/ecommerce/product/'+data)
+const getCategories = await useLazyFetch('https://gama.soluve.cloud/categories', {
+    params: { 'per_page': 100, 'page': 1 }
+})
+const categories = getCategories.data
+let pageNumber: number = Number(route.params.slug)
+let products= {}
+if(pageNumber>=1){
+    products = await useFetch('https://gama.soluve.cloud/products', {
+    params: { 'per_page': 50, 'page': pageNumber }
+})
+
+} else {
+    products = await useFetch('https://gama.soluve.cloud/products', {
+    params: { 'per_page': 50, 'page': 1 }
+})
 }
 
-const {products}: any = await productsNavigation(pageNumber)
+console.log(products)
+async function productsPagination(data: any) {
+    if(data<=0){
+        return navigateTo('/apps/ecommerce/productlist')
+    }
+    return navigateTo('/apps/ecommerce/product/' + data)
+}
 </script>
 
 <template>
@@ -50,14 +55,15 @@ const {products}: any = await productsNavigation(pageNumber)
     <!-- Table Editable -->
     <!-- ---------------------------------------------------- -->
     <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
+    {{ pageN }}
     <v-row>
         <v-col cols="12">
             <UiParentCard title="Editable Table" class=" my-2">
                 <EditableTable :products="products.data" :categories="toRaw(categories)" />
                 <div class="d-flex justify-end my-5 mr-5 gap-2">
-                    <v-btn color="info" variant="flat" >Previose page</v-btn>
-                    <v-btn color="info" variant="flat" disabled >{{pageNumber}}</v-btn>
-                    <v-btn color="info" variant="flat" @click="productsPagination((pageNumber+1))">Next page</v-btn>
+                    <v-btn color="info" variant="flat" @click="productsPagination((pageNumber - 1))" >Previose page</v-btn>
+                    <v-btn color="info" variant="flat" disabled>{{ pageNumber }}</v-btn>
+                    <v-btn color="info" variant="flat" @click="productsPagination((pageNumber + 1))">Next page</v-btn>
                 </div>
 
             </UiParentCard>
